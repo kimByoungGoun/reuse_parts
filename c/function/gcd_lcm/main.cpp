acb296fd3ed9592385f8bc3a990c19a7dae18c0c
kgun610 <kgun610@naver.com>
2021-06-04 11:37:10 +0900

    gcd_lcm

---
+++ c/function/gcd_lcm/main.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int get_gcd(int a, int b) {
+    while (b != 0) {
+        int r = a % b;
+        a = b;
+        b = r;
+    }
+    return a;
+}
+
+int get_lcm(int a, int b, int c) {
+    return a * b / c;
+}
+
+int main() {
+
+    int a, b, gcd, lcm;
+
+    cin >> a >> b;
+
+    gcd = get_gcd(a, b);
+    lcm = get_lcm(a, b, gcd);
+
+    cout << gcd << endl << lcm << endl;
+
+    return 0;
+}
